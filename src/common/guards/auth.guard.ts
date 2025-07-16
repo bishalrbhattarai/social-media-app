2b8a2d0c43184988e28fbfc90efc03cb55dea2e9
bishalrbhattarai <bishalrajbhattarai01@gmail.com>
2025-07-16 11:55:13 +0545

    auth.guard changes verification status checked

--- src/common/guards/auth.guard.ts
+++ src/common/guards/auth.guard.ts
@@ -8,6 +8,7 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 import { GqlContext } from 'src/auth/resolvers/auth.resolver';
 import { TokenService } from '../services/token.service';
 import { CacheService } from '../services/cache.service';
+import { ForbiddenError } from '@nestjs/apollo';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -22,15 +23,19 @@ export class AuthGuard implements CanActivate {
     const token = request.headers['authorization']?.split(' ')[1];
     // const token = request.headers['authorization'];
 
-
     if (!token) throw new UnauthorizedException('Token is Missing');
     const payload = this.tokenService.verifyAccessToken(token);
     if (!payload) throw new UnauthorizedException('Invalid Token');
+
+    if (!payload.isEmailVerified)
+      throw new ForbiddenError('Email is not verified');
+
     const cacheValue = await this.cacheService.get<string>(
       `access-token:${payload.jti}`,
     );
 
     if (!cacheValue) throw new UnauthorizedException('Unauthorized User');
+
     if (cacheValue !== token)
       throw new UnauthorizedException('Unauthorized User');
     request.user = { ...payload, _id: payload.sub };
