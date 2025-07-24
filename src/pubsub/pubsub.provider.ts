8fc64fe791947c24a7728b9c795728a3d102cbb6
bishalrbhattarai <bishalrajbhattarai01@gmail.com>
2025-07-24 13:49:53 +0545

    changes

--- src/pubsub/pubsub.provider.ts
+++ src/pubsub/pubsub.provider.ts
@@ -2,13 +2,16 @@ import { Provider } from '@nestjs/common';
 import { RedisPubSub } from 'graphql-redis-subscriptions';
 import Redis, { RedisOptions } from 'ioredis';
 
+const HOST = '127.0.0.1';
+const PORT = 6379;
+
 const redisOptions: RedisOptions = {
-  host: '127.0.0.1',
-  port: 6379,
+  host: HOST,
+  port: PORT,
   retryStrategy: (times) => Math.min(times * 50, 2000),
 };
 
-export const PUB_SUB = 'PUB_SUB'; 
+export const PUB_SUB = 'PUB_SUB';
 
 export const PubSubProvider: Provider = {
   provide: PUB_SUB,
